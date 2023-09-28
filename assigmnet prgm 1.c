c149c93db18ba8ffcdc0b0bf879646315cb76a8d
Samarththakuri <143748639+Samarththakuri@users.noreply.github.com>
2023-09-28 19:09:25 +0530

    Add files via upload

---
+++ assigmnet prgm 1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int r;// radius of circle 
+    printf("Enter the readius :");
+    scanf("%d",&r);
+    int area;
+    area= 3.14*r*r;
+    printf("The area of circle is=%d ",area);
+    return 0;
+
+}
