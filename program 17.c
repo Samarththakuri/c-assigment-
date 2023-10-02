fc06deaf2aa26d37e9c3c3a6a459378b1e621d1a
Samarththakuri <143748639+Samarththakuri@users.noreply.github.com>
2023-10-02 17:43:37 +0530

    Add files via upload

---
+++ program 17.c
@@ -0,0 +1,12 @@
+//• Checking Number Is Even or Odd
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    if(n%2==0)
+    printf("The number is even");
+    else
+    printf("the number is odd");
+    return 0;
+}
